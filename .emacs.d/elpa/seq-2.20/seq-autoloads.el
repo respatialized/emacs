17854a1c4406cc63c2ba460dc3c4ecd97292c654
Andrew Foltz-Morrison <andrew.foltz-morrison@sitomobile.com>
2017-08-08 10:54:56 -0400

    switching to prelude with first whitespace customization

--- .emacs.d/elpa/seq-2.20/seq-autoloads.el
+++ .emacs.d/elpa/seq-2.20/seq-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil nil ("seq-24.el" "seq-25.el" "seq-pkg.el" "seq.el")
-;;;;;;  (22918 23026 310668 863000))
+;;;;;;  (22921 51100 855405 828000))
 
 ;;;***
 

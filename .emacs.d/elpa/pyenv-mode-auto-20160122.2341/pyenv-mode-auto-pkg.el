c4ff917d208a0d1f5216130c935d58c90eb0e535
Andrew Foltz-Morrison <afoltzm@gmail.com>
2017-10-02 09:35:44 -0400

    adding ob-ipython functionality

---
+++ .emacs.d/elpa/pyenv-mode-auto-20160122.2341/pyenv-mode-auto-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "pyenv-mode-auto" "20160122.2341" "Automatically activates pyenv version if .python-version file exists." '((pyenv-mode "0.1.0") (s "1.11.0") (f "0.17.0")) :commit "714e322319ad7a8f5f2a3691aa431537b8a1b26c" :url "https://github.com/ssbb/pyenv-mode-auto" :keywords '("python" "pyenv"))

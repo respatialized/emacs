8c058a231f3d0e3873d5850de8ce6de35fb47122
Andrew Foltz-Morrison <afoltzm@gmail.com>
2017-11-03 07:41:56 -0400

    major update: xah-fly-keys, org-brain, anaconda, deft, and many more

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -10,6 +10,23 @@
   (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/")))
 (package-initialize)
 
+
+;; El-Get
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (require 'package)
+  (add-to-list 'package-archives
+               '("melpa" . "http://melpa.org/packages/"))
+  (package-refresh-contents)
+  (package-initialize)
+  (package-install 'el-get)
+  (require 'el-get))
+
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+(el-get 'sync)
+
+
 ;; Xah-Fly-Keys setup
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 (require 'xah-fly-keys)
@@ -42,10 +59,10 @@
  '(conda-anaconda-home "/home/andrew/.conda")
  '(custom-safe-themes
    (quote
-    ("15348febfa2266c4def59a08ef2846f6032c0797f001d7b9148f30ace0d08bcf" "1db337246ebc9c083be0d728f8d20913a0f46edc0a00277746ba411c149d7fe5" default)))
+    ("67e998c3c23fe24ed0fb92b9de75011b92f35d3e89344157ae0d544d50a63a72" "15348febfa2266c4def59a08ef2846f6032c0797f001d7b9148f30ace0d08bcf" "1db337246ebc9c083be0d728f8d20913a0f46edc0a00277746ba411c149d7fe5" default)))
  '(package-selected-packages
    (quote
-    (cider conda pyenv-mode-auto anaconda-mode pyenv-mode s dash-functional dash flatui-theme zenburn-theme xah-fly-keys ample-zen-theme))))
+    (org-capture-pop-frame org-brain deft el-get which-key conda pyenv-mode-auto anaconda-mode pyenv-mode s dash-functional dash flatui-theme zenburn-theme xah-fly-keys ample-zen-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -75,7 +92,30 @@
 ;; if you want auto-activation (see below for details), include:
 (conda-env-autoactivate-mode t)
 
-; ob-clojure
-(setq org-babel-clojure-backend 'cider)
-(require 'ob-clojure)
-(require 'cider)
+
+(add-to-list
+  'el-get-sources
+  '(:name spotify.el
+          :type github
+          :pkgname "danielfm/spotify.el"
+          :description "Control the Spotify app from within Emacs"
+          :url "https://github.com/danielfm/spotify.el"
+          :after (progn
+                  (setq spotify-oauth2-client-secret "9d7b65831e3d40148b055ea9e3472d07")
+                  (setq spotify-oauth2-client-id "4aeb25c7700947a0b579d3fa5fd34f76"))))
+
+; Org-Brain / Deft / Information
+
+(setq org-brain-path "~/org/brain")
+(setq org-id-track-globally t)
+(setq org-id-locations-file "~/.emacs.d/.org-id-locations")
+
+(require 'deft)
+
+(defun org-brain-deft ()
+  "Use `deft' for files in `org-brain-path'."
+  (interactive)
+  (let ((deft-directory org-brain-path)
+        (deft-recursive t)
+        (deft-extensions '("org")))
+    (deft)))

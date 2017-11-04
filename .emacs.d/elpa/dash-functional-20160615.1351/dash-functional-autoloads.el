cdf784881744857fec0ff6536d46af7ad07e1339
Andrew Foltz-Morrison <afoltzm@gmail.com>
2017-11-04 14:24:41 -0400

    minimizing repo by switching to org-based init file and list of packages

--- .emacs.d/elpa/dash-functional-20160615.1351/dash-functional-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; dash-functional-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("dash-functional.el") (22993 23736 931544
-;;;;;;  427000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dash-functional-autoloads.el ends here
--- .emacs.d/elpa/org-capture-pop-frame-20160518.308/org-capture-pop-frame-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "org-capture-pop-frame" "20160518.308" "Run org-capture in a new pop frame" '((emacs "24.4")) :commit "b16fd712de62cf0d1f9befd03be6ab5983cb3301" :url "https://github.com/tumashu/org-capture-pop-frame.git")

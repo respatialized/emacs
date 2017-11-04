337b7579a791ce54a660845e6e6736a8b37f6f12
Andrew Foltz-Morrison <afoltzm@gmail.com>
2017-11-04 14:24:41 -0400

    minimizing repo by switching to org-based init file and list of packages

--- .emacs.d/elpa/conda-20171028.617/conda-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "conda" "20171028.617" "Work with your conda environments" '((emacs "24.4") (pythonic "0.1.0") (dash "2.13.0") (s "1.11.0") (f "0.18.2")) :commit "526be691824f4a32299f560a883913697bc4d847" :url "http://github.com/necaris/conda.el" :keywords '("python" "environment" "conda"))
--- .emacs.d/elpa/pyenv-mode-auto-20160122.2341/pyenv-mode-auto-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; pyenv-mode-auto-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("pyenv-mode-auto.el") (22993 29251 331273
-;;;;;;  451000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; pyenv-mode-auto-autoloads.el ends here
